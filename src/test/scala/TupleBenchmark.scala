f85028e20bdd7976edd1d6a0c20a4e92a7bb7291
clhodapp <clhodapp1@gmail.com>
2013-05-11 01:34:05 -0500

    Don't include creation time in access benchmark

--- src/test/scala/TupleBenchmark.scala
+++ src/test/scala/TupleBenchmark.scala
@@ -25,33 +25,48 @@ object TupleBenchmark extends PerformanceTest.Microbenchmark {
 			}
 		}
 
-		measure method "apply(1,2,3)._1" in {
-			using (repetitions) in { r =>
-				for(_ <- 1 to r) Tuple(1,2,3)
+		{
+			val t = Tuple(1,2,3)
+			measure method "apply(1,2,3)._1" in {
+				using (repetitions) in { r =>
+					for(_ <- 1 to r) t._1
+				}
 			}
 		}
 
-		measure method "apply(1,2,3,1.2)._1" in {
-			using (repetitions) in { r =>
-				for (_ <- 1 to r) Tuple(1,2,3,1.2)._1
+		{
+			val t = Tuple(1,2,3,1.2)
+			measure method "apply(1,2,3,1.2)._1" in {
+				using (repetitions) in { r =>
+					for (_ <- 1 to r) t._1
+				}
 			}
 		}
 
-		measure method """apply(1,2,"x",3,1.2,"a")._1""" in {
-			using (repetitions) in { r =>
-				for (_ <- 1 to r) Tuple(1,2,"x",3,1.2,"a")._1
+		{
+			val t = Tuple(1,2,"x",3,1.2,"a")
+			measure method """apply(1,2,"x",3,1.2,"a")._1""" in {
+				using (repetitions) in { r =>
+					for (_ <- 1 to r) t._1
+				}
 			}
 		}
 
-		measure method """apply(1,2,"x",3,1.2,"a")._3""" in {
-			using (repetitions) in { r =>
-				for (_ <- 1 to r) Tuple(1,2,"x",3,1.2,"a")._3
+		{
+			val t = Tuple(1,2,"x",3,1.2,"a")
+			measure method """apply(1,2,"x",3,1.2,"a")._3""" in {
+				using (repetitions) in { r =>
+					for (_ <- 1 to r) t._3
+				}
 			}
 		}
 
-		measure method """apply(1,2,"x",3,1,2,("a": Any))._3""" in {
-			using (repetitions) in { r =>
-				for (_ <- 1 to r) Tuple(1,2,"x",3,1.2,("a": Any))._3
+		{
+			val t = Tuple(1,2,"x",3,1.2,("a": Any))
+			measure method """apply(1,2,"x",3,1,2,("a": Any))._3""" in {
+				using (repetitions) in { r =>
+					for (_ <- 1 to r) t._3
+				}
 			}
 		}
 
@@ -78,32 +93,37 @@ object TupleBenchmark extends PerformanceTest.Microbenchmark {
 		}
 
 		measure method "apply(1,2,3)._1" in {
+			val t = (1,2,3)
 			using (repetitions) in { r =>
-				for(_ <- 1 to r) (1,2,3)._1
+				for(_ <- 1 to r) t._1
 			}
 		}
 
 		measure method "apply(1,2,3,1.2)._1" in {
+			val t = (1,2,3,1.2)
 			using (repetitions) in { r =>
-				for (_ <- 1 to r) (1,2,3,1.2)._1
+				for (_ <- 1 to r) t._1
 			}
 		}
 
 		measure method """apply(1,2,"x",3,1.2,"a")._1""" in {
+			val t = (1,2,"x",3,1.2,"a")
 			using (repetitions) in { r =>
-				for (_ <- 1 to r) (1,2,"x",3,1.2,"a")._1
+				for (_ <- 1 to r) t._1
 			}
 		}
 
 		measure method """apply(1,2,"x",3,1.2,"a")._3""" in {
+			val t = (1,2,"x",3,1.2,"a")
 			using (repetitions) in { r =>
-				for (_ <- 1 to r) (1,2,"x",3,1.2,"a")._3
+				for (_ <- 1 to r) t._3
 			}
 		}
 
 		measure method """apply(1,2,"x",3,1.2,("a": Any))._3""" in {
+			val t = (1,2,"x",3,1.2,("a": Any))
 			using (repetitions) in { r =>
-				for (_ <- 1 to r) (1,2,"x",3,1.2,("a": Any))._3
+				for (_ <- 1 to r) t._3
 			}
 		}
 
